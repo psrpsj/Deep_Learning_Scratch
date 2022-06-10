50a1d966b247bc404ad629ef4bac2fad86c64a33
Sangryu Park <psrpsj@gmail.com>
2022-06-10 22:55:32 +0900

    add optimizer.py

---
+++ Convolution Neural Network/optimizer.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class SGD:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key]
+
+
+class AdaGrad:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h == None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[key]) + 1e-7)
+
+
+# class Adam:
